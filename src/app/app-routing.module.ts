5e18add1c2ecdf61c33ce58b27b1e1c00ebe973f
zhagun <zhagunpv@gmail.com>
2022-08-17 20:42:49 +0300

    feat: add main page layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
+import { MainPageComponent } from './pages/main-page/main-page.component';
+import { JoinPageComponent } from './pages/join-page/join-page.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainPageComponent },
+  { path: 'join', component: JoinPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './pages/main-page/main-page.component';
+import { JoinPageComponent } from './pages/join-page/join-page.component';
+import { LogoComponent } from './components/logo/logo.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    JoinPageComponent,
+    LogoComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
